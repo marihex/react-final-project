6c3ef78e2a4cb066c3150a90c57091f31e4b3528
rieznyk131 <m.rieznyk@gmail.com>
2026-06-09 22:44:54 +0200

    add styles to the header add carousel to the home page

--- src/components/carousel/CarouselComponent.tsx
+++ src/components/carousel/CarouselComponent.tsx
@@ -21,74 +21,21 @@ export const CarouselComponent = () => {
     return (
         <section className='w-[60%] m-auto pt-4 overflow-hidden  rounded-lg'>
             {
-                upcoming.length > 0 && <CCarousel controls indicators>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100  rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[0].backdrop_path}`}
-                            alt="slide 1"/>
-                    <CCarouselCaption className="hero-caption">
-                        <h5><Link to={`/movie/${movieCarousel[0].id}`}>{movieCarousel[0].title}</Link></h5>
-                        <p>{dateFormatHelper(movieCarousel[0].release_date)}</p>
-                        <p><StarIcon
-                            style={{ color: 'gold' , fontSize: 15}}
-                        /> {movieCarousel[0].vote_average.toFixed(1)}</p>
-                    </CCarouselCaption>
-                </CCarouselItem>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100 rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[1].backdrop_path}`}
-                            alt="slide 2"/>
-                        <CCarouselCaption className="hero-caption">
-                        <h5>{movieCarousel[1].title}</h5>
-                        <p>{dateFormatHelper(movieCarousel[1].release_date)}</p>
-                        <p><StarIcon
-                            style={{ color: 'gold' , fontSize: 15}}
-                        /> {movieCarousel[1].vote_average.toFixed(1)}</p>
-                    </CCarouselCaption>
-                </CCarouselItem>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100 rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[2].backdrop_path}`}
-                                alt="slide 2"/>
-                        <CCarouselCaption className="hero-caption">
-                            <h5>{movieCarousel[2].title}</h5>
-                            <p>{dateFormatHelper(movieCarousel[2].release_date)}</p>
-                            <p><StarIcon
-                                style={{ color: 'gold' , fontSize: 15}}
-                            /> {movieCarousel[2].vote_average.toFixed(1)}</p>
-                        </CCarouselCaption>
-                    </CCarouselItem>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100 rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[3].backdrop_path}`}
-                                alt="slide 2"/>
-                        <CCarouselCaption className="hero-caption">
-                            <h5>{movieCarousel[3].title}</h5>
-                            <p>{dateFormatHelper(movieCarousel[3].release_date)}</p>
-                            <p><StarIcon
-                                style={{ color: 'gold' , fontSize: 15}}
-                            /> {movieCarousel[3].vote_average.toFixed(1)}</p>
-                        </CCarouselCaption>
-                    </CCarouselItem>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100 rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[4].backdrop_path}`}
-                                alt="slide 2"/>
-                        <CCarouselCaption className="hero-caption">
-                            <h5>{movieCarousel[4].title}</h5>
-                            <p>{dateFormatHelper(movieCarousel[4].release_date)}</p>
-                            <p><StarIcon
-                                style={{ color: 'gold' , fontSize: 15}}
-                            /> {movieCarousel[4].vote_average.toFixed(1)}</p>
-                        </CCarouselCaption>
-                    </CCarouselItem>
-                    <CCarouselItem className="hero-slide">
-                    <CImage className="d-block w-100 rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movieCarousel[5].backdrop_path}`}
-                                alt="slide 2"/>
-                        <CCarouselCaption className="hero-caption">
-                            <h5>{movieCarousel[5].title}</h5>
-                            <p>{dateFormatHelper(movieCarousel[5].release_date)}</p>
-                            <p><StarIcon
-                                style={{ color: 'gold' , fontSize: 15}}
-                            /> {movieCarousel[5].vote_average.toFixed(1)}</p>
-                        </CCarouselCaption>
-                    </CCarouselItem>
-
+                upcoming.length > 0 &&
+                <CCarousel controls indicators>
+                    {movieCarousel.map(movie =>
+                        <CCarouselItem className="hero-slide">
+                            <CImage className="d-block w-100  rounded-lg" src={`${imgBaseUrl}${sizeUrl}${movie.backdrop_path}`}
+                                    alt="slide 1"/>
+                            <CCarouselCaption className="hero-caption">
+                                <h5><Link to={`/movie/${movie.id}`}>{movie.title}</Link></h5>
+                                <p>{dateFormatHelper(movie.release_date)}</p>
+                                <p><StarIcon
+                                    style={{ color: 'gold' , fontSize: 15}}
+                                /> {movie.vote_average.toFixed(1)}</p>
+                            </CCarouselCaption>
+                        </CCarouselItem>
+                    )}
             </CCarousel>
             }
         </section>
